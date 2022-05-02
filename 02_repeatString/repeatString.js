a624dfb37b4ecca4b766b4c72a9986acfb957279
Diego <dpardoblacutt@gmail.com>
2022-05-02 13:08:57 -0400

    Add repeatString() that returns a string concated given a num and a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    const repeatedStrings = [];
+    for(let i = 0; i<num;i++){
+        repeatedStrings.push(string);
+    }
+    return repeatedStrings.join("");
 };
 
 // Do not edit below this line
