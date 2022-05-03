caf02e7d760d3483d9d3110836ceb6a3d4b7368a
Diego <dpardoblacutt@gmail.com>
2022-05-03 19:09:47 -0400

    Add conditionals to know if a year is leap or not leap dividing it on three main functions divisible by 4, 100 and 400

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,22 @@
-const leapYears = function() {
-
+const LEAP = true;
+const NOT_LEAP = false;
+const leapYears = function(year) {
+    
+    if (isDivisibleBy4(year)===false){
+        return NOT_LEAP;
+    }else if(isDivisibleBy4(year)===true && 
+             isDivisibleBy100(year)===false && 
+             isDivisibleBy400(year)===false){
+        return LEAP;
+    }else if(isDivisibleBy4(year)===true && 
+             isDivisibleBy100(year)===true && 
+             isDivisibleBy400(year)===false){
+        return NOT_LEAP;
+    }else if(isDivisibleBy4(year)===true && 
+             isDivisibleBy100(year)===true && 
+             isDivisibleBy400(year)===true){
+        return LEAP;
+    }
 };
 
 // Do not edit below this line
