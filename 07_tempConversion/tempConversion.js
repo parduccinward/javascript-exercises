9f1e93cac112c521af493a9971f65f6f1a0f768a
Diego <dpardoblacutt@gmail.com>
2022-05-03 19:39:59 -0400

    Add ftoc() and ctof() functions to converts from celsius to fahrenheit and viceversa. Also add a roundNumber() function to get one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  return roundNumber(5/9*(fahrenheit-32));
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  return roundNumber(9/5*celsius)+32;
 };
+
+function roundNumber(number){
+  return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
