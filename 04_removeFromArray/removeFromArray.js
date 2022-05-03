6fc2a036d577d8b56f0d3b435034e7a0a9f7ea85
Diego <dpardoblacutt@gmail.com>
2022-05-03 16:51:55 -0400

    Add removeFromArray() that separates arguments into an array and all arguments, then iterates over it and searches for equals and return it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
 const removeFromArray = function() {
+    const allArguments = Array.from(arguments);
+    const arrayToShrink = allArguments.shift(); //exclude first argument that is the array
+    let argumentsToRemove = allArguments;
 
+    for (let i = 0; i<argumentsToRemove.length; i++) {
+        for(let j = 0; j<arrayToShrink.length; j++){
+            if(argumentsToRemove[i] === arrayToShrink[j]){
+                arrayToShrink.splice(j,1);
+                break;
+            }
+        }
+    }
+    return arrayToShrink;
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
