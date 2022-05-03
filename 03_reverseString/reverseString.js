750f686a4be06a1b33d6dda982bfdc7f968f9ce5
Diego <dpardoblacutt@gmail.com>
2022-05-03 10:26:33 -0400

    Add reverseString() applying a loop to iterate over the string to create and array with reversed characters

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringCharacters = [];
+    let stringLength = string.length;
+    for (let i =0; i<stringLength; i++) {
+        let lastCharacter = string.slice(-1);
+        stringCharacters.push(lastCharacter);
+        string = string.slice(0,string.length-1);
+    }
+    return stringCharacters.join('');
 };
 
 // Do not edit below this line
