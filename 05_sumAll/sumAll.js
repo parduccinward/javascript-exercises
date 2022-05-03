111fc9b39b6153d72f06cdc297d246ffece2735f
Diego <dpardoblacutt@gmail.com>
2022-05-03 18:37:09 -0400

    Add function that sums between two intervals x and y and displays ERROR message if NaN

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let numbers = Array.from(arguments);
+    if(validateNumbers(numbers) === false) return "ERROR";
+    biggerNumber = getBiggerNumber(numbers);
+    smallerNumber = getSmallerNumber(numbers);
+    iterationSteps = (biggerNumber-smallerNumber)+1;
+    let resultNumber=0;
+    for(i=smallerNumber;i<=iterationSteps;i++){
+        resultNumber+=i;
+    }
+    return resultNumber;
 };
+
+function validateNumbers(array){
+    for(const number of array){
+        if(number<0 || typeof number=="string" || typeof number=="object"){
+            return false;
+        }
+    }
+}
+
+function getBiggerNumber(array){
+        return (array[0]>array[1]) ? array[0] : array [1];
+}
+
+function getSmallerNumber(array){
+    return (array[0]<array[1]) ? array[0] : array [1];
+}
 
 // Do not edit below this line
 module.exports = sumAll;
