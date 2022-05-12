8cdaeab6028e51376e72e44eec73e9f745284814
Diego <dpardoblacutt@gmail.com>
2022-05-12 19:39:04 -0400

    Add a function that checks if a person is still living then updates that person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    if(checkStillLiving(people)) updateArray(people);
+    const oldest = people.sort((a,b) => {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })
+    //console.table(oldest);
+    return oldest[0];
 };
+
+function getThisYear(){
+    let today = new Date();
+    return today.getFullYear();
+}
+
+function checkStillLiving(people){
+    for (const person of people) {
+        if(!("yearOfDeath" in person)) return true;
+    }
+    return false;
+}
+
+function updateArray(people){
+    let thisYear = getThisYear();
+    for (const person of people) {
+        if(!("yearOfDeath" in person)) person.yearOfDeath = thisYear;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
