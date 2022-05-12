da0645ee06aff234fd082e133b01feb835cf4f40
Diego <dpardoblacutt@gmail.com>
2022-05-12 16:59:31 -0400

    Add a function that removes punctuation characters, then reverse a string using arrays and returns true if the string is palindrome or false it is not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const inputString = removePunctuationCharacters(string).toLowerCase()
+    const reversedString = reverseString(inputString).toLowerCase();
+    return (inputString === reversedString) ? true : false;
 };
+
+function createStringArray(string){
+    const stringArray = [];
+    fillArrayWithCharacters(stringArray, string);
+    return stringArray
+}
+
+function fillArrayWithCharacters(stringArray, string){
+    for (let i = 0; i < string.length; i++) {
+        stringArray.push(string.charAt(i));
+    }
+}
+
+function reverseString(string){
+    const stringArray = createStringArray(string);
+    const reversedStringArray = stringArray.reverse();
+    return reversedStringArray.join('');
+}
+
+function removePunctuationCharacters(string){
+    return string.replace(/[^\p{L}]/gu,"");
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
