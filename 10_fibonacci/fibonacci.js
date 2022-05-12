231cb6913918bea5fa5ee37a85d2783cae19df89
Diego <dpardoblacutt@gmail.com>
2022-05-12 18:08:29 -0400

    Add for loop that iterates over fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number<0) return "OOPS";
+    if (number == 1 || number == 2) return 1;
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    for(i=0;i<number;i++){
+        c = a+b;
+        a = b;
+        b = c;
+    }
+    return a;
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
